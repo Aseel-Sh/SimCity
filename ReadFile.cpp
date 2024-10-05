bb900d9044ee5e8e62c30f0faa31ed7835171b82
Ronnie Perez <RonnieCodes95@gmail.com>
2024-10-05 11:46:16 -0500

    Created ReadFile.cpp for tests on opening and reading the csv file/s

---
+++ ReadFile.cpp
@@ -0,0 +1,19 @@
+// Going to make the function for extracting the data for the csv file in here
+// We can later import it to main or just copy paste it into the main code onece its working
+// - Ronnie
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+void openFile() {
+    
+}
+void readFile() {
+    
+}
+int main() {
+
+}
