39a86d36432791291ad9bd128cb578780177bdb0
Kyilee Bell <KyileeBell@my.unt.edu>
2024-11-01 12:55:25 -0500

    Created Residential.h and Residential.cpp, with added constuctor and grow function in Residential.h

---
+++ Residential.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "Residential.h"
+
+
+Residential::Residential(int x, int y, Region* region) {
+    
+}
---
+++ Residential.h
@@ -0,0 +1,46 @@
+#ifndef RESIDENTIAL_H
+#define RESIDENTIAL_H
+
+#include "Region.h"
+
+
+class Residential: public Cell {
+
+    public:
+    // Constructor
+    Residential(int x, int y, Region* region);
+
+
+    private:
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // RESIDENTIAL_H
